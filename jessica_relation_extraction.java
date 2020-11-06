3346f44109d9d68e8509b14d4a0ff7a5cdec49a9
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-11-06 17:57:35 +0400

    Update jessica_relation_extraction.java

--- jessica_relation_extraction.java
+++ jessica_relation_extraction.java
@@ -1,16 +1,16 @@
 /*
-for file in `find . -name "*.jar"`; do export CLASSPATH="$CLASSPATH:`realpath $file`"; done
-
-javac jessica_relation_extraction.java
-java jessica_relation_extraction
-
 https://stackoverflow.com/questions/20754129/how-to-call-java-from-python-using-py4j
 
 https://stanfordnlp.github.io/CoreNLP/kbp.html#list-of-relations
-*/
 
-/*
 https://nlp.stanford.edu/nlp/javadoc/javanlp/edu/stanford/nlp/pipeline/CoreDocument.html
+
+http://manmustbecool.github.io/MyWiki/Wiki/Python/python_java.html
+
+for file in `find . -name "*.jar"`; do export CLASSPATH="$CLASSPATH:`realpath $file`"; done
+
+javac jessica_relation_extraction.java
+jar -cvf jessica_relation_extraction.jar jessica_relation_extraction.class
 */
 
 import java.util.*;
@@ -20,36 +20,46 @@ import edu.stanford.nlp.pipeline.*;
 
 public class jessica_relation_extraction {
 
-	public static void main(String[] args) {
+	public static StanfordCoreNLP pipeline;
+
+	public jessica_relation_extraction(){
+		long startTime;
+		long elapsedTime;
+
+		startTime = System.nanoTime();
 		Properties props = new Properties();
 		props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,depparse,kbp");
-		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		pipeline = new StanfordCoreNLP(props);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("model laoding time: "+ elapsedTime/1000000000 +" seconds.");	
+	}
 
-		CoreDocument document = pipeline.processToCoreDocument("Smith's wife is Jessica. He study in the New York University.");
+	public static List<String> relation_extraction(String text){
+		CoreDocument document = pipeline.processToCoreDocument(text);
 		List<CoreSentence> s = document.sentences();
 
 		CoreSentence s1;
 		List<RelationTriple> tripltes;
 		List<CoreEntityMention> entities;
+		List<String> relation_triplets = new ArrayList<>();
 
-		int sentence_len = s.size();
-		for(int i =0; i < sentence_len; i++){
+		for(int i =0; i < s.size(); i++){
 			s1 = s.get(i);
 			tripltes = s1.relations();
-			int triplet_num = tripltes.size();
-			for(int j=0;j<triplet_num;j++){
+			for(int j=0;j<tripltes.size();j++){
 				RelationTriple t = tripltes.get(j);
-				String output = String.format("%s:%s -[%s]-> %s:%s (%f)", 
+				String output = String.format("%s:%s -[%s]-> %s:%s (%f) | %s", 
 					t.subjectGloss(), 
 					t.subjectHead().ner(),
 					t.relationGloss(), 
 					t.objectGloss(), 
 					t.objectHead().ner(),
-					t.confidence);  
-				System.out.println(output);				
+					t.confidence,
+					s1.text());  
+				relation_triplets.add(output);		
 			}
 		}
-
+		return relation_triplets;
 	}
 
 }
