fe11076ad62e20fb5c27a736de31ce35300b35c1
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-11-06 18:31:33 +0400

    Update jessica_relation_extraction.py

--- jessica_relation_extraction.py
+++ jessica_relation_extraction.py
@@ -1,5 +1,6 @@
 ##########jessica_relation_extraction.py##########
 import os
+import re
 from py4j.java_gateway import JavaGateway
 
 os.system(u"""
@@ -7,13 +8,21 @@ os.system(u"""
 	java jessica_relation_extraction &
 	""")
 
-gateway = JavaGateway()
-random = gateway.jvm.java.util.Random()
-number1 = random.nextInt(10)
+while True:
+	try:
+		gateway = JavaGateway()
+		random = gateway.jvm.java.util.Random()
+		number1 = random.nextInt(10)
+		break
+	except:
+		pass
 
 jessica = gateway.jvm.jessica_relation_extraction('kbp')
-relation = jessica.relation_extraction("Smith's wife is Jessica. Jessica is working for Apple. Jessica is 23 years old.")
 
-for r in relation:
-	print(r)
+def relation_extraction(text):
+	relation = jessica.relation_extraction(text)
+	outputs = [
+	re.search(r'^(?P<subject_name>.*)\:(?P<subject_type>[^a-z]+) \-\[(?P<relation>.*)\]-> (?P<object_name>.*)\:(?P<object_type>[^a-z]+) \((?P<confidence>[\d\.]+)\) \| (?P<setence>.*)$', r).groupdict()
+	for r in relation]
+	return outputs
 ##########jessica_relation_extraction.py##########
